4ff4b67ed3cdaba85cd1a7c46a547c7bdc839a93
cnrd <hello@conradweise.com>
2019-06-01 18:48:43 +0200

    init :---)

---
+++ build_dataset.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import argparse
+import re
+
+import xml.etree.ElementTree as ET
+
+def build(data_dir):
+  def get_filenames():
+    for (_, _, filenames) in os.walk(data_dir):
+      return filenames
+  
+  def get_contents(xml_file):
+    tree = ET.parse(os.path.join(data_dir, xml_file))
+    root = tree.getroot()
+    posts = []
+    comments = []
+    for post in root.iter('item'):
+      for post_content in post.iter('{http://purl.org/rss/1.0/modules/content/}encoded'):
+        posts.append(post_content.text)
+      for comment_content in post.iter('{http://wordpress.org/export/1.2/}comment_content'):
+        comments.append(comment_content.text)
+    return posts, comments
+  
+  def clean(string):
+    # no_tags = re.compile('(<.*?>)|(\\n)|(\\t)|(&.*?;)|([^A-Za-z0-9 ])')
+    first = re.compile('(<.*?>)|(&.*?;)|(\\n)|(\\t)|(http.*)')
+    second = re.compile('([^A-Za-z0-9 ])')
+    third = re.compile(' {2,}')
+    cleaned = first.sub(' ', string)
+    cleaned = second.sub('', cleaned)
+    cleaned = third.sub(' ', cleaned)
+    return cleaned
+
+  def save(string):
+    with open('dataset.txt', 'w+') as f:
+      f.write(string)
+
+  dataset = ''
+  files = get_filenames()
+  for f in files:
+    posts, comments = get_contents(f)
+    content = ''.join(posts + comments)
+    dataset = dataset + content
+  
+  cleaned = clean(dataset)
+  save(cleaned)
+
+def main():
+  current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--data_dir',
+      type=str,
+      default=os.path.join(current_path, 'data'),
+      help='The data directory to process Wordpress post exports.')
+  flags, unused_flags = parser.parse_known_args()
+  build(flags.data_dir)
+
+main()
